1d2137d21b202449907b09ef6b1f7a22a0a126f1
Bowen <bowen_show@163.com>
2014-11-12 14:29:45 +0800

    latest

--- Smartbus/src/com/hesong/smartbus/client/FlowInvokeMode.java
+++ Smartbus/src/com/hesong/smartbus/client/FlowInvokeMode.java
@@ -1,21 +1,6 @@
 package com.hesong.smartbus.client;
 
-/**
- * 流程调用模式
- * 
- * @see com.hesong.smartbus.client.net.Client#invokeFlow
- * 调用流程时候，需要指定流程调用的方式，是需要返回值的流程调用还是不需要返回值的流程调用。
- * @author tanbro
- * 
- */
 public enum FlowInvokeMode {
-	/**
-	 * 需要返回值
-	 */
-	HASRESULT(0),
-	/**
-	 * 不需要返回值
-	 */
 	NORESULT(1);
 
 	private int value;
@@ -24,9 +9,6 @@ public enum FlowInvokeMode {
 		this.value = value;
 	}
 
-	/**
-	 * 返回该枚举所对应的整数值。
-	 */
 	public int getValue() {
 		return this.value;
 	}
